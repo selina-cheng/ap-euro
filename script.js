16640ac51dc7ed43a03475bc5932f37d9fc48b18
Selina Cheng <66575815+selina-cheng@users.noreply.github.com>
2021-04-10 23:26:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+function changeImage(topic) {
+  switch (topic) {
+    case "ir2":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2F2nd%20industrial%20rev.png?v=1618113584449";
+      break;
+    case "crimwar":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fcrimean%20war%20and%20italy%20unification.png?v=1618115603882";
+      break;
+    case "geruni":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fgermany%20unification.png?v=1618119078078";
+      break;
+    case "karlmarx":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fkarl%20marx%2C%20science%20and%20culture.png?v=1618119079656";
+      break;
+    case "nstate":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fnational%20state.png?v=1618119080919";
+      break;
+    case "imp":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fimperialism.png?v=1618119941387";
+      break;
+    case "scimod":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fmodern%20consciousness.png?v=1618119942567";
+      break;
+    case "poli":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fpolitics.png?v=1618119944597";
+      break;
+    case "prewwi":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fbefore%20wwi.png?v=1618119940265";
+      break;
+    case "cwwi":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fthe%20road%20to%20wwi.png?v=1618120580639";
+      break;
+    case "wwi":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fthe%20wwi.png?v=1618120582423";
+      break;
+    case "rrev":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Frussian%20rev.png?v=1618120584065";
+      break;
+    case "pwwi":
+      document.getElementById("notes").src =
+        "https://cdn.glitch.com/e9ea0f69-9d30-44f4-b64c-cb26bfd597b0%2Fpeace%20settlement.png?v=1618120584884";
+      break;
+  }
+}
